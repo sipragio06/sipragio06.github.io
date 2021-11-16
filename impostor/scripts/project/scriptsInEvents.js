aa006eff248f510b76258ddf51b619eb90619c2c
mazahacka2017 <mazahacka2017@gmail.com>
2021-11-16 17:30:04 +0400

    impostor

---
+++ impostor/scripts/project/scriptsInEvents.js
@@ -0,0 +1,38 @@
+"use strict";
+
+
+
+{
+	const scriptsInEvents = {
+
+		async MainMenu_Event25(runtime, localVars)
+		{
+			function CreateRoom()
+			{
+				//place here code needed to go to Create Room page
+			}
+			
+		},
+
+		async Endgame_Event19(runtime, localVars)
+		{
+			function ReturnToTitle()
+			{
+				//place here code needed to actually return to title page
+			}
+			
+		},
+
+		async Settings_Event76(runtime, localVars)
+		{
+			function LaunchGame()
+			{
+				//place here code needed to actually launch the game
+			}
+			
+		}
+
+	};
+	
+	self.C3.ScriptsInEvents = scriptsInEvents;
+}
